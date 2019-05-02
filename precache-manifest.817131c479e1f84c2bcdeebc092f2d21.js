64f63ceb138c84e46e85d516debe07df636f56e4
Khalilurrehman <khalil_rehman1993@ymail.com>
2019-05-02 22:37:47 +0400

    Updates

---
+++ precache-manifest.817131c479e1f84c2bcdeebc092f2d21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bcc0c221e5824ee16109e35e966ae63",
+    "url": "/TravelSort/index.html"
+  },
+  {
+    "revision": "b39d3a077b67351a792a",
+    "url": "/TravelSort/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8e1d570b912513aeaf63",
+    "url": "/TravelSort/static/css/main.fcf3ea83.chunk.css"
+  },
+  {
+    "revision": "b39d3a077b67351a792a",
+    "url": "/TravelSort/static/js/2.7db56f7f.chunk.js"
+  },
+  {
+    "revision": "8e1d570b912513aeaf63",
+    "url": "/TravelSort/static/js/main.e30f61eb.chunk.js"
+  },
+  {
+    "revision": "182b84d6e9d1ae328e16",
+    "url": "/TravelSort/static/js/runtime~main.84a4d306.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TravelSort/static/media/logo.5d5d9eef.svg"
+  }
+]);
